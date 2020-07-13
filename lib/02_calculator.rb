53d9aeec95a1d0ca6ce5f062fe3e7a0f0746ead0
Raphael LUTZ <lutz.raphael@icloud.com>
2020-07-13 21:16:40 +0200

    sauvegarde 02

---
+++ lib/02_calculator.rb
@@ -0,0 +1,30 @@
+def add(x,y)
+	addresult = (x.to_i + y.to_i)
+	addresult
+end
+
+
+
+def substract(a,b)
+	substractresult = (a.to_i) - (b.to_i)
+	sumresult
+end 
+
+
+def sum(c,d)
+	sumresult = (c.to_i + d.to_i)
+	sumresult
+end
+
+def mutiply(e,f)
+	mutiplyresult = (c.to_i * d.to_i)
+	mutiplyresult
+end
+
+
+def power 
+end
+
+
+def factorial
+end
